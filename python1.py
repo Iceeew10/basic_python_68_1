76bf2ae5b91db4300e4178a0dc0e423765086a21
Aiyaman Wansu <aiyaman@email.com>
2026-01-31 13:17:46 +0700

    Python variable

---
+++ python1.py
@@ -0,0 +1,39 @@
+"""
+#
+#long comment
+#
+"""
+
+# single line comment (short comment)
+
+'''
+print("Hello World")
+print("Hello World")
+
+x = 10
+y = 20
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+'''
+
+x = str(10)
+y = int(10)
+z = float(10)
+print(x)
+print(y)
+print(z)
+
+# การตั้งชื่อตัวแปร variable
+
+myvar = 100
+my_var = 100
+_myvar = 100
+myVar = 100
+MYVAR = 100
+myvar1 = 100
+
+myVariableName = 100
+MyVariableName = 100
+my_variable_name = 100
